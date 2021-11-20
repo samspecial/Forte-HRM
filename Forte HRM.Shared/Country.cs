a2e5daa5e9ff0b3bc286486e1fe828df880a372a
Samuel Osinloye <psalmueloye@gmail.com>
2021-11-21 00:00:08 +0100

    Add project files.

---
+++ Forte HRM.Shared/Country.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Forte_HRM.Shared
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Forte HRM.Shared/MaritalStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Forte_HRM.Shared
+{
+    public enum MaritalStatus
+    {
+        Married,
+        Single,
+        Other
+    }
+}
